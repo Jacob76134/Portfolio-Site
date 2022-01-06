6c758148944b30a97551daf7f1d8642395225682
Jacob Brewer <jacobbrewerrooney@gmail.com>
2022-01-06 15:36:55 -0500

    github links and new card

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 const panels = document.querySelectorAll('.panel')
+const hoverInfo = document.querySelector('.hover-info')
+const hoverTitle = document.querySelector('.hover-title')
+const blurb = document.querySelector('.blurb')
+const gitLink = document.getElementById('gitlink')
 
 
+// Panel Animation
 panels.forEach(panel => {
     panel.addEventListener('mouseover', () => {
         removeActiveClasses ()
@@ -21,3 +26,22 @@ function removeActiveClasses () {
         panel.classList.remove('active')
     })
 }
+
+// Info Update
+document.getElementById('weather-dashboard').addEventListener('mouseover', () => {
+    hoverTitle.textContent = 'Weather Dashboard';
+    gitLink.href = 'https://github.com/Jacob76134/Weather-Dashboard';
+    blurb.textContent = 'Definitely my favorite project to date. Using data from a weather API and displaying it in different ways is simply delightful';
+})
+
+document.getElementById('cyclist').addEventListener('mouseover', () => {
+    hoverTitle.textContent = 'Cyclist';
+    gitLink.href = 'https://wl0194.github.io/Cyclist/';
+    blurb.textContent = 'One of the more challenging group projects as we had to pivot our development direction on short notice, very happy with the result';
+})
+
+document.getElementById('horiseon').addEventListener('mouseover', () => {
+    hoverTitle.textContent = 'Horiseon';
+    gitLink.href = 'https://jacob76134.github.io/refactoring-html-project/';
+    blurb.textContent = 'A fun and simple site with a large showcase that shows the power of HTML and CSS';
+})
